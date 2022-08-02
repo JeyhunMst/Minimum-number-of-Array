85c2652c3b1ea2a1aa82c84027dc84012ed10f06
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-03 01:56:41 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinNumbOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 213, 43, 12, 4, 21, 423, 232, 1, 3, 0, 3 };
+            int ind = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[ind]>arr[i])
+                {
+                    ind = i;
+                }
+            }
+            Console.WriteLine($"Minimum number's index is {ind} and it is equal to {arr[ind]} ");
+        }   
+    }
+}
